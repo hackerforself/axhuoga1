6a939676e022aaff85b1d69d6ab0f69e0ff6f573
axhuoga <axihuoga@gmail.com>
2024-11-16 13:53:32 +0800

    Add files via upload

---
+++ syteryu.cpp
@@ -0,0 +1,26 @@
+﻿
+#include<iostream>
+using namespace std;
+int main() {
+    int a, b;
+    char s;
+    cout << "请输入一组算式:" << endl;
+    for (;;)
+    {
+        cin >> a >> s >> b;
+        if (s == '+') {
+            cout << a << "+" << b << "=" << a + b<<endl;
+        }
+        else if (s == '-') {
+            cout << a << "-" << b << "=" << a - b<<endl;
+        }
+        else if (s == '*') {
+            cout << a << "*" << b << "=" << a * b << endl;
+        }
+        else if (s == '/') {
+            cout << a << "/" << b << "=" << a / b<<endl;
+        }
+    }
+    return 0;
+}
+  
