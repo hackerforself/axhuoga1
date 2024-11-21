858a82a5987b4e8babf8413d21088fbab148982d
axhuoga <axihuoga@gmail.com>
2024-11-21 22:01:13 +0800

    Create lieshu.cpp

---
+++ lieshu.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int a;
+	cout<<"Please enter any number:"<<endl;
+	cin>>a;
+	for(int i=1;i<=a;i++){
+		cout<<i<<endl;	
+	}
+	return 0;
+}
