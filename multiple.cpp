b7e6e98b57cd36c84052e906803546575df94e88
hackerforself <hackerforself@gmail.com>
2024-11-22 10:00:10 +0800

    Update multiple.cpp
    I known you want to list a number's multiple in range 100
    if you want to list a number's multiple for other range?
    so you should define other viarable to specify the range, so your program will be best

--- multiple.cpp
+++ multiple.cpp
@@ -1,12 +1,21 @@
+/*
+* I known you want to list a number's multiple in range 100
+* if you want to list a number's multiple for other range?
+* so you should define other viarable to specify the range,
+* so your program will be best
+*/
+
 #include<iostream>
 using namespace std;
 int main(){
     int a;
-    cin>>a;
-    for(int i=a;i<=100;i++){
-		if(i%a==0){
-			cout<<i<<endl;
-		}
+    int range; //define range
+    cin>>a>>range; //read a and range from console
+    
+    for(int i=a;i<=range;i++){
+	if(i%a==0){
+		cout<<i<<endl;
 	}
+}
     return 0;
 }
